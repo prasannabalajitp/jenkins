613101e0015c84f5dfa7a97604339b92d7b5f097
prasannabalajitp <61289440+prasannabalajitp@users.noreply.github.com>
2023-07-06 14:56:04 +0530

    Update prasanna.sh

--- prasanna.sh
+++ prasanna.sh
@@ -1,3 +1,19 @@
 echo "Hello, Prasanna T P "
 echo "Today is Thursday"
 echo "Thanks!!!"
+
+echo "Enter Size(N)"
+read N
+
+i=1
+sum=0
+
+echo "Enter Numbers"
+while [ $i -le $N ]
+do
+  read num           #get number
+  sum=$((sum + num)) #sum+=num
+  i=$((i + 1))
+done
+
+echo $sum
