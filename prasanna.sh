ef9907220975c2623c25a6998acd3627cac48de4
prasannabalajitp <61289440+prasannabalajitp@users.noreply.github.com>
2023-10-23 12:49:28 +0530

    Update prasanna.sh

--- prasanna.sh
+++ prasanna.sh
@@ -4,15 +4,15 @@ echo "Thanks!!!"
 
 
 
-i=1
-sum=0
+# i=1
+# sum=0
 
-echo "Enter Numbers"
-while [ $i -le 6 ]
-do
-  read num           #get number
-  sum=$((sum + num)) #sum+=num
-  i=$((i + 1))
-done
+# echo "Enter Numbers"
+# while [ $i -le 6 ]
+# do
+#   read num           #get number
+#   sum=$((sum + num)) #sum+=num
+#   i=$((i + 1))
+# done
 
-echo $sum
+# echo $sum
